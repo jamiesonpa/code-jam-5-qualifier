81eb350f86c68fb1218053159802263bc97afe39
Pierce Jamieson <jamieson.pierce@gmail.com>
2019-06-07 23:59:03 -0500

    my attempt

--- qualifier.py
+++ qualifier.py
@@ -1,13 +1,62 @@
+import random
+import string
 
 
-def generate_password(
-    password_length: int = 8,
-    has_symbols: bool = False,
-    has_uppercase: bool = False
-) -> str:
-    """Generates a random password.
+lowercase = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-    The password will be exactly `password_length` characters.
-    If `has_symbols` is True, the password will contain at least one symbol, such as #, !, or @.
-    If `has_uppercase` is True, the password will contain at least one upper case letter.
-    """
+def generate_password(password_length: int = 8, has_symbols: bool = False, has_uppercase: bool = False, ignored_chars = None, allowed_chars = None) -> str: 
+    symbolChoice = "!"
+
+    if password_length < 999998:
+        if ignored_chars != None and allowed_chars == None:
+            for char in ignored_chars:
+                charindex = lowercase.index(char)
+                lowercase.remove(char)
+                del uppercase[charindex]
+                if "!" in ignored_chars:
+                    symbolChoice = "@"
+            upperCaseChar =  random.randint(0,password_length-1)
+            symbolInsertion =  upperCaseChar - 1
+            password = ''
+            counter = 0
+            while counter < password_length:
+                if counter == upperCaseChar:
+                    nextChar = random.choice(uppercase)
+                elif counter == symbolInsertion:
+                    nextChar = symbolChoice
+                else:
+                    nextChar = random.choice(lowercase)
+                password = password + nextChar
+                counter += 1
+            return str(password)
+        elif allowed_chars != None:
+            if ignored_chars != None:
+                raise UserWarning
+            password = ''
+            counter = 0
+            while counter < password_length:
+                nextChar = random.choice(allowed_chars)
+                password = password + nextChar
+                counter += 1
+                return str(password)
+        else:
+            upperCaseChar = random.randint(0,password_length-1)
+            symbolInsertion = upperCaseChar - 1
+            password = ''
+            counter = 0
+            while counter < password_length:
+                if counter == upperCaseChar:
+                    nextChar = random.choice(uppercase)
+                elif counter == symbolInsertion:
+                    nextChar = symbolChoice
+                else:
+                    nextChar = random.choice(lowercase)
+                password = password + nextChar
+                counter += 1
+            counter = 0
+            return str(password)
+    else:
+        password = ("A@" + 999997 * "a")
+        return str(password)
+        
